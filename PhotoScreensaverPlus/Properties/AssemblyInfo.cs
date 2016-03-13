f0001b43e257f390d4d8292131b6a3c04d3e36e8
Kamil Svoboda <kamil.svoboda@centrum.cz>
2016-03-13 16:32:24 +0100

    version 1.9

--- PhotoScreensaverPlus/Properties/AssemblyInfo.cs
+++ PhotoScreensaverPlus/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kamil Svoboda")]
 [assembly: AssemblyProduct("Photo Screensaver Plus")]
-[assembly: AssemblyCopyright("Copyright ©  2008-2015")]
+[assembly: AssemblyCopyright("Copyright ©  2008-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
